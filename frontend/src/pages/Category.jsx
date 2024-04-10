f6ba343fa4548542cb5a0655dc76bb1063feca28
edm1001 <edmer_franciz@yahoo.com>
2024-04-09 21:49:36 -0500

    add filterby comp

--- frontend/src/pages/Category.jsx
+++ frontend/src/pages/Category.jsx
@@ -1,27 +1,62 @@
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
 
 function Category() {
-  return (
-    <section className="flex h-screen bg-gray-100 ">
-      {/* left filter card */}
-      <div className="shadow-md left-0 ">
-      Filter by
-      <form>
-        <input
-        type="range" 
-        min="0" 
-        max="100" 
-        // value={}
-        // onChange={}
-        />
-        {/* add other filters */}
-         {/*quicklinks  */}
-        {/* rating */}
-      </form>
-      </div>
+  const [rating, setRating] = useState(0);
 
-      {/* add blog type list for items make an component import */}
-    </section>
-  )
+  const handleStarClick = (value) => {
+    setRating(value === rating ? 0 : value);
+  };
+  const renderStars = () => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      stars.push(
+        <FaStar
+          key={i}
+          className={`text-${
+            i <= rating ? "yellow" : "gray"
+          }-500 cursor-pointer`}
+          onClick={() => handleStarClick(i)}
+        />
+      );
+    }
+    return stars;
+  };
+
+  return (
+    <div className="absolute left-0 m-12 p-4 w-72 ">
+      <div className="shadow-md bg-gray-100 p-6">
+        <h3 className="font-bold text-xl">Filter by</h3>
+        <form>
+          <div className="mb-2">
+            <label className="block mb-1 text-md font-bold">Price Range:</label>
+            <div>
+              <input type="checkbox" className="mr-2" id="priceRange1" />
+              <label htmlFor="priceRange1">$0-$25</label>
+            </div>
+            <div>
+              <input type="checkbox" className="mr-2" id="priceRange1" />
+              <label htmlFor="priceRange1">$25-$75</label>
+            </div>
+            <div>
+              <input type="checkbox" className="mr-2" id="priceRange1" />
+              <label htmlFor="priceRange1">$0-$25</label>
+            </div>
+            <div>
+              <input type="checkbox" className="mr-2" id="priceRange1" />
+              <label htmlFor="priceRange1">$100+</label>
+            </div>
+          </div>
+
+          {/* Add more filter options here */}
+          <div className="mb-4">
+            <label className="block mb-1">Rating:</label>
+            <div className="flex">{renderStars()}</div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Category
+export default Category;
