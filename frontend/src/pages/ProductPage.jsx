e7b587f00c7cac5b66148ba3443a9787a2001a6a
Edmer Valencia <edmerfranciz@gmail.com>
2024-06-17 17:06:34 -0500

    edit json

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -23,15 +23,25 @@ const ProductPage = () => {
 
   return (
     <section className="mt-16">
-
-    <div className="text-black h-screen bg-blue text-center">      
-      <p>Name: {product.name}</p>
-      <img src={product.image} alt={product.name} className="w-1/2 h-auto object-cover" />
-      <p>Description: {product.description}</p>
-      <p>Price: ${product.price}</p>
-      <p>Company: {product.company}</p>
+    <div className="flex flex-col h-full min-h-screen p-12 bg-gradient-to-r from-gray-100 to-gray-200">
+      <h1 className="text-3xl text-blue mb-4">{product.name}</h1>
+      <p className="text-sm font-semibold text-darkblue">Company: {product.company}</p>
+      <p className="text-end mr-4 text-black">${product.price}</p>
+      <hr className="mb-8 border-1 border-darkblue"/>
+      <div className="flex flex-col md:flex-row items-center md:items-start md:space-x-8">
+        {/* Gallery feature for images can go here */}
+        <img src={product.image} alt={product.name} className="w-full md:w-1/2 h-auto rounded-lg shadow-sm mb-8 md:mb-0"/>
+        <div className="md:flex-1">
+          <p className="text-sm text-black">{product.description}</p>
+          <div className="mt-8 flex space-x-4 justify-end">
+            <button className="bg-darkblue text-white px-4 py-2 rounded hover:opacity-50">Buy Now</button>
+            <button className="bg-darkblue text-white px-4 py-2 rounded hover:opacity-50">Add to Cart</button>
+          </div>
+        </div>
+      </div>
     </div>
-    </section>
+  </section>
+  
   );
 };
 
