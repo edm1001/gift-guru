da8a0b203f585c5c384e7878b25f1b719fef403d
Edmer Valencia <edmerfranciz@gmail.com>
2024-06-20 17:00:45 -0500

    update json

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,35 +9,81 @@ function Navbar() {
     setIsOpen(!isOpen);
   };
   return (
-    <div className="fixed top-0 left-0 right-0  p-4 z-50 shadow-md">
-      <div className="flex justify-between items-center">
-        <div className="text-white font-bold text-2xl"><Link to="/">Gift Guide</Link></div>
+    <div className="fixed top-0 left-0 right-0  z-50 shadow-md">
+      <div className="flex justify-between items-center p-4 bg-darkblue opacity-80">
+        <div className="text-white font-bold text-2xl">
+          <Link to="/">Gift Guide</Link>
+        </div>
         <div className="sm:hidden">
           <Hamburger
             isOpen={isOpen}
             menuClicked={toggleMenu}
-            width={18}
-            height={15}
+            width={20}
+            height={14}
             strokeWidth={2}
             rotate={0}
-            color="black"
+            color="lightblue"
             borderRadius={0}
             animationDuration={0.5}
           />
         </div>
-        <nav className="hidden sm:flex space-x-4 text-sm">
-          <Link className="font-bold py-2 px-4 hover:opacity-50 focus:text-lightblue focus:border focus:ring-lightblue" to="/shop">Gifts</Link>
-          <Link className="font-bold py-2 px-4 hover:opacity-50 focus:text-lightblue focus:border focus:ring-lightblue" to="/blog">Trending</Link>
-          <Link className="font-bold py-2 px-4 hover:opacity-50 focus:text-lightblue focus:border focus:ring-lightblue" to="/links">Quicklinks</Link>
-          <Link className="font-bold py-2 px-4 hover:opacity-50 focus:text-lightblue focus:border focus:ring-lightblue" to="/quiz">Discover</Link>
+        <nav className="hidden sm:flex space-x-4 text-sm text-lightblue">
+          <Link
+            className="font-bold py-2 px-4 hover:opacity-50 focus:text-lightblue focus:border focus:ring-lightblue"
+            to="/shop"
+          >
+            Gifts
+          </Link>
+          <Link
+            className="font-bold py-2 px-4 hover:opacity-50 focus:text-lightblue focus:border focus:ring-lightblue"
+            to="/blog"
+          >
+            Trending
+          </Link>
+          <Link
+            className="font-bold py-2 px-4 hover:opacity-50 focus:text-lightblue focus:border focus:ring-lightblue"
+            to="/links"
+          >
+            Quicklinks
+          </Link>
+          <Link
+            className="font-bold py-2 px-4 hover:opacity-50 focus:text-lightblue focus:border focus:ring-lightblue"
+            to="/quiz"
+          >
+            Discover
+          </Link>
         </nav>
       </div>
       {isOpen && (
-        <nav className="sm:hidden bg-brown-900  shadow-sm p-4 text-center">
-          <Link className="block font-bold py-2 px-4 hover:opacity-50 hover:text-lightblue" to="/shop" onClick={toggleMenu}>Gifts</Link>
-          <Link className="block font-bold py-2 px-4 hover:opacity-50 hover:text-lightblue" to="/blog" onClick={toggleMenu}>Trending</Link>
-          <Link className="block font-bold py-2 px-4 hover:opacity-50 hover:text-lightblue" to="/links" onClick={toggleMenu}>Quicklinks</Link>
-          <Link className="block font-bold py-2 px-4 hover:opacity-50 hover:text-lightblue" to="/quiz" onClick={toggleMenu}>Discover</Link>
+        <nav className="sm:hidden pb-2 shadow-sm text-center text-lightblue bg-darkblue opacity-80 rounded-b-md">
+          <Link
+            className="block font-bold py-2 px-4 hover:opacity-50 hover:text-lightblue"
+            to="/shop"
+            onClick={toggleMenu}
+          >
+            Gifts
+          </Link>
+          <Link
+            className="block font-bold py-2 px-4 hover:opacity-50 hover:text-lightblue"
+            to="/blog"
+            onClick={toggleMenu}
+          >
+            Trending
+          </Link>
+          <Link
+            className="block font-bold py-2 px-4 hover:opacity-50 hover:text-lightblue"
+            to="/links"
+            onClick={toggleMenu}
+          >
+            Quicklinks
+          </Link>
+          <Link
+            className="block font-bold py-2 px-4 hover:opacity-50 hover:text-lightblue"
+            to="/quiz"
+            onClick={toggleMenu}
+          >
+            Discover
+          </Link>
         </nav>
       )}
     </div>
