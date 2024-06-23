a48d36a485a7c8d434b1b74fd51c0110611ce681
Edmer Valencia <edmerfranciz@gmail.com>
2024-06-22 23:44:56 -0500

    add to json

---
+++ frontend/src/pages/ContactPage.jsx
@@ -0,0 +1,13 @@
+import { About } from '../components/About';
+
+// TODO: add c contact page to email, add about component at the bottom
+const ContactPage = () => {
+  return (
+    <section className='mt-16'>
+    <div>ContactPage</div>
+    <About/>
+    </section>
+  )
+}
+
+export default ContactPage
--- frontend/src/pages/LinksPage.jsx
+++ frontend/src/pages/LinksPage.jsx
@@ -1,5 +1,21 @@
+import React from "react"
 
+const quickLinksData = [
+  { id: 'tech', name: 'For Tech Geeks'},
+  { id: 'nature', name: 'For Nature Lovers'},
+  {id:'food', name: 'For Home Chefs'},
+  {id:'fitness', name: 'For Gym Goers'},
+  {id:'books', name: 'For Bookworms'},
+  {id:'pets', name: 'For Pet Owners'},
+  {id:'music', name: 'For Music Lovers'},
+  {id:'diy', name: 'For DIY Enthusiasts'},
+  {id:'art', name: 'For Art Aficionados'},
+  {id:'games', name: 'For the Gamers'},
+  {id:'fashion', name: 'For the Fashionistas'},
+  {id:'travel', name: 'For Travel Addicts'},
+]
 function LinksPage() {
+
   return (
     <div className="bg-blue">
       <h3>Quick links</h3>
