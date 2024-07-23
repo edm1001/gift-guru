4dfd63795603108a4ee29b375156febde3c407ac
Edmer Valencia <edmerfranciz@gmail.com>
2024-07-22 23:06:53 -0500

    fix slider bug

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,12 +1,12 @@
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Hero = () => {
   const images = [
-    'https://source.unsplash.com/random/1600x900/?nature',
-    'https://source.unsplash.com/random/1600x900/?water',
-    'https://source.unsplash.com/random/1600x900/?forest',
+    "https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/247599/pexels-photo-247599.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://plus.unsplash.com/premium_vector-1689096905639-a1d0a2e1bed5?bg=FFFFFF&w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8N3xzZU5jQ296Z3lyTXx8ZW58MHx8fHx8",
   ];
 
   const settings = {
@@ -20,14 +20,17 @@ const Hero = () => {
   };
 
   return (
-    <div className="h-screen w-full">
+    <div className="w-full">
       <Slider {...settings} className="h-full">
         {images.map((image, index) => (
-          <div key={index} className="h-full">
-            <div
-              className="h-full w-full bg-cover bg-center"
-              style={{ backgroundImage: `url(${image})` }}
-            ></div>
+          <div key={index}>
+            <div className="flex justify-center items-center h-full">
+              <img
+                src={image}
+                alt={`Slide ${index}`}
+                className="h-full w-full object-cover"
+              />
+            </div>
           </div>
         ))}
       </Slider>
@@ -36,4 +39,3 @@ const Hero = () => {
 };
 
 export default Hero;
-
