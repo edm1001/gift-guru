b3b7b22199800316f9bdbed2e89a28731bf11b30
Edmer Valencia <edmerfranciz@gmail.com>
2024-07-27 23:51:32 -0500

    add banner link

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,37 +3,34 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="bg-gray-200 p-4 text-center">
-    {/* Create a row for all footer items */}
-    <div className="flex flex-wrap justify-around items-center text-sm space-x-2 sm:space-x-4">
-      <div className="flex-grow p-2 sm:p-4 hover:text-blue hover:underline cursor-pointer">
-        <Link
-        to="/">
-          Home
-          </Link>
-          </div>
-      <div className="flex-grow p-2 sm:p-4 hover:text-blue hover:underline cursor-pointer">
-        <Link to="/shop" >
-        Products
-        </Link>
+      {/* Create a row for all footer items */}
+      <div className="flex flex-wrap justify-around items-center text-sm space-x-2 sm:space-x-4">
+        <div className="flex-grow p-2 sm:p-4 hover:text-blue hover:underline cursor-pointer">
+          <Link to="/">Home</Link>
         </div>
-      <div className="flex-grow p-2 sm:p-4 hover:text-blue hover:underline cursor-pointer">
-        <Link to="/links">
-        Personalized Gifts
-        </Link>
+        <div className="flex-grow p-2 sm:p-4 hover:text-blue hover:underline cursor-pointer">
+          <Link to="/shop">Products</Link>
         </div>
-      <div className="flex-grow p-2 sm:p-4 hover:text-blue hover:underline cursor-pointer">
-        <Link to="/contact">
-        Contact Us
-        </Link>
+        <div className="flex-grow p-2 sm:p-4 hover:text-blue hover:underline cursor-pointer">
+          <Link to="/links">Personalized Gifts</Link>
         </div>
-    </div>
+        <div className="flex-grow p-2 sm:p-4 hover:text-blue hover:underline cursor-pointer">
+          <Link to="/contact">Contact Us</Link>
+        </div>
+      </div>
       <div className="flex-grow p-2 sm:p-4 flex items-center space-x-2 justify-center">
         <p className="text-gray-700">Follow us:</p>
-        <FaYoutube size={20} color="red" className="hover:scale-125 cursor-pointer" />
+        <FaYoutube
+          size={20}
+          color="red"
+          className="hover:scale-125 cursor-pointer"
+        />
         <FaTiktok size={14} className="hover:scale-125 cursor-pointer" />
       </div>
-    <p className="text-xs sm:text-sm text-gray-700 mt-4">&copy; 2024 GiftGuide. All rights reserved.</p>
-  </div>
+      <p className="text-xs sm:text-sm text-gray-700 mt-4">
+        &copy; 2024 GiftGuide. All rights reserved.
+      </p>
+    </div>
   );
 };
 
--- frontend/src/pages/LinksPage.jsx
+++ frontend/src/pages/LinksPage.jsx
@@ -1,39 +1,40 @@
-import { useState, useEffect } from "react"
-
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const quickLinksData = [
-  { id: 'tech', name: 'For Tech Geeks'},
-  { id: 'nature', name: 'For Nature Lovers'},
-  {id:'food', name: 'For Home Chefs'},
-  {id:'fitness', name: 'For Gym Goers'},
-  {id:'books', name: 'For Bookworms'},
-  {id:'pets', name: 'For Pet Owners'},
-  {id:'music', name: 'For Music Lovers'},
-  {id:'diy', name: 'For DIY Enthusiasts'},
-  {id:'art', name: 'For Art Aficionados'},
-  {id:'games', name: 'For the Gamers'},
-  {id:'fashion', name: 'For the Fashionistas'},
-  {id:'travel', name: 'For Travel Addicts'},
-]
+  { id: "tech", name: "For Tech Geeks" },
+  { id: "nature", name: "For Nature Lovers" },
+  { id: "food", name: "For Home Chefs" },
+  { id: "fitness", name: "For Gym Goers" },
+  { id: "books", name: "For Bookworms" },
+  { id: "pets", name: "For Pet Owners" },
+  { id: "music", name: "For Music Lovers" },
+  { id: "diy", name: "For DIY Enthusiasts" },
+  { id: "art", name: "For Art Aficionados" },
+  { id: "games", name: "For the Gamers" },
+  { id: "fashion", name: "For the Fashionistas" },
+  { id: "travel", name: "For Travel Addicts" },
+];
 // TODO: Fix cards because its not grabbing data correctly
 
 // QuickLinks Component
 const QuickLinks = ({ onLinkClick }) => {
   return (
     <div className="quick-links-container p-2">
-        <h1 className="mb-8 text-center text-grey text-2xl">Quick Links</h1>
+      <h1 className="mb-8 text-center text-grey text-2xl">Quick Links</h1>
       <div className="grid grid-cols-3 md:grid-cols-4 h-full gap-4">
         {quickLinksData.map((link) => (
           <div
-          key={link.id}
-          className="bg-gray-200 rounded-lg hover:scale-105 hover:ring-4 hover:ring-grey hover:scale-125 flex justify-center">
-          <button
-            onClick={() => onLinkClick(link.id)}
-            className="quick-link-card px-4 py-4 sm:py-12 rounded-lg hover:opacity-70 transition-shadow"
+            key={link.id}
+            className="bg-gray-200 rounded-lg hover:scale-105 hover:ring-4 hover:ring-grey hover:scale-125 flex justify-center"
+          >
+            <button
+              onClick={() => onLinkClick(link.id)}
+              className="quick-link-card px-4 py-4 sm:py-12 rounded-lg hover:opacity-70 transition-shadow"
             >
-            <h3 className="text-2xxs md:text-lg">{link.name}</h3>
-          </button>
-            </div>
+              <h3 className="text-2xxs md:text-lg">{link.name}</h3>
+            </button>
+          </div>
         ))}
       </div>
     </div>
@@ -46,17 +47,53 @@ const ProductList = ({ products }) => {
     <div className="product-list p-4">
       <h1 className="mb-4 text-center text-xl text-grey h-96">Products</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {products.map(product => (
-          <div key={product.id} className="product-card p-4 rounded-lg shadow-md">
-            <img src={product.images[0].url} alt={product.images[0].alt} className="w-full h-auto rounded-lg mb-4" />
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="product-card p-4 rounded-lg shadow-md"
+          >
+            <img
+              src={product.images[0].url}
+              alt={product.images[0].alt}
+              className="w-full h-auto rounded-lg mb-4"
+            />
             <h3 className="text-xl font-bold">{product.name}</h3>
             <p className="text-gray-600">{product.description}</p>
-            <p className="text-gray-800 font-semibold">${product.price.toFixed(2)}</p>
-            <a href={product.affiliateLink} className="text-white hover:underline">Buy Now</a>
+            <p className="text-gray-800 font-semibold">
+              ${product.price.toFixed(2)}
+            </p>
+            <a
+              href={product.affiliateLink}
+              className="text-white hover:underline"
+            >
+              Buy Now
+            </a>
           </div>
         ))}
       </div>
     </div>
+  );
+};
+const QuizBanner = () => {
+  return (
+    <div className="flex flex-col md:flex-row items-center bg-white p-4 rounded-lg">
+    <div className="md:w-2/3 p-4">
+      <h1 className="text-xl font-semibold mb-2 text-darkblue">
+        Discover Your Perfect Gift!
+      </h1>
+      <p className="text-gray-700">
+        Take our quiz to get personalized gift recommendations.
+      </p>
+    </div>
+    <div className="md:w-1/3 bg-darkblue p-4 text-center rounded-lg hover:bg-lightblue transition duration-200">
+      <Link
+        to="/quiz"
+        className="text-lg font-semibold text-white hover:text-blue-900"
+      >
+        Take Our Quiz!
+      </Link>
+    </div>
+  </div>
   );
 };
 
@@ -73,28 +110,34 @@ const LinksPage = () => {
     // Fetch product data
     const fetchProducts = async () => {
       try {
-        const response = await fetch('/api/links');
+        const response = await fetch("/api/links");
         if (!response.ok) {
           throw new Error(`Failed to fetch products: ${response.status}`);
-      }
-      const data = await response.json();
+        }
+        const data = await response.json();
 
-      // Filter products based on selectedLinkId
-      if (selectedLinkId) {
-        const filtered = data.categories.flatMap(category =>
-          category.subcategories.flatMap(subcategory =>
-            subcategory.products.filter(product => product.linkIds.includes(selectedLinkId))
-          )
-        );
-        setFilteredProducts(filtered);
-      } else {
-        setFilteredProducts(data.categories.flatMap(category =>
-          category.subcategories.flatMap(subcategory => subcategory.products)
-        ));
+        // Filter products based on selectedLinkId
+        if (selectedLinkId) {
+          const filtered = data.categories.flatMap((category) =>
+            category.subcategories.flatMap((subcategory) =>
+              subcategory.products.filter((product) =>
+                product.linkIds.includes(selectedLinkId)
+              )
+            )
+          );
+          setFilteredProducts(filtered);
+        } else {
+          setFilteredProducts(
+            data.categories.flatMap((category) =>
+              category.subcategories.flatMap(
+                (subcategory) => subcategory.products
+              )
+            )
+          );
+        }
+      } catch (error) {
+        console.error("Error fetching products:", error);
       }
-    } catch (error) {
-      console.error('Error fetching products:', error);
-    }
     };
 
     fetchProducts();
@@ -104,6 +147,7 @@ const LinksPage = () => {
     <div className="mt-16 bg-white p-8">
       <QuickLinks onLinkClick={handleLinkClick} />
       <ProductList products={filteredProducts} />
+      <QuizBanner />
     </div>
   );
 };
