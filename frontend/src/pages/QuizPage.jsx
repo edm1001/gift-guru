03469ac00bb09bb68932a928fd1979996f880ed5
Edmer Valencia <edmerfranciz@gmail.com>
2024-07-31 23:53:53 -0500

    update quiz page

--- frontend/src/pages/QuizPage.jsx
+++ frontend/src/pages/QuizPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-// import categories from '../db/Shop/Categories.json';
 import questions from '../db/Question.json';
+// import categories from '../db/Shop/Categories.json';
 
 const QuizPage = () => {
   const [quizStarted, setQuizStarted] = useState(false);
@@ -8,33 +8,37 @@ const QuizPage = () => {
   const [answers, setAnswers] = useState({});
 
   const handleAnswer = (answer) => {
-    // Save the answer for the current question
-    setAnswers(prevAnswers => ({ ...prevAnswers, [currentQuestion]: answer }));
-
-    // Move to the next question or end the quiz if there are no more questions
-    if (currentQuestion < questions.length - 1) {
-      setCurrentQuestion(currentQuestion + 1);
-    } else {
-      // Quiz completed
-      setQuizStarted(false); 
-      alert('Quiz completed!');
-    }
+    // Save the answer
+    setAnswers(prevAnswers => {
+      const newAnswers = { ...prevAnswers, [currentQuestion]: answer };
+      // Move to the next question
+      if (currentQuestion < questions.length - 1) {
+        setCurrentQuestion(prevIndex => prevIndex + 1);
+      } else {
+        setQuizStarted(false); // End quiz
+      }
+      return newAnswers;
+    });
   };
 
   return (
     <section className="bg-gray-200 min-h-screen pt-16 pb-8 text-center">
       <div className="bg-gradient-to-r from-gray-100 to-lightblue h-screen flex flex-col justify-center items-center">
-        <h2 className="text-center text-black text-4xl font-semibold">Quiz Page</h2>
-        <p className='mt-1 text-xs'>Answer the following questions to proceed!</p>
+        {!quizStarted && (
+          <>
+            <h2 className="text-center text-black text-4xl font-semibold">Quiz Page</h2>
+            <p className='mt-1 text-xs'>Answer the following questions to find personalized products!</p>
+          </>
+        )}
         {quizStarted ? (
           currentQuestion < questions.length ? (
             <div>
-              <p className="text-xl font-semibold">{questions[currentQuestion].question}</p>
+              <p className="text-2xl mb-4">{questions[currentQuestion].question}</p>
               {questions[currentQuestion].options.map((option, index) => (
                 <button
                   key={index}
                   onClick={() => handleAnswer(option)}
-                  className="bg-darkblue text-white p-2 rounded m-2"
+                  className="bg-blue text-white p-2 rounded m-2 hover:opacity-60"
                 >
                   {option}
                 </button>
