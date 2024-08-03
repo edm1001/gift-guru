1ba869f180193b24f1ffe688faa67a4ef5f6fe2c
Edmer Valencia <edmerfranciz@gmail.com>
2024-08-02 22:33:26 -0500

    add back button

--- frontend/src/pages/QuizPage.jsx
+++ frontend/src/pages/QuizPage.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
-import questions from '../db/Question.json';
+import { useState } from "react";
+import questions from "../db/Question.json";
 // import categories from '../db/Shop/Categories.json';
+import { MdOutlineKeyboardBackspace } from "react-icons/md";
 
 const QuizPage = () => {
   const [quizStarted, setQuizStarted] = useState(false);
@@ -8,50 +9,74 @@ const QuizPage = () => {
   const [answers, setAnswers] = useState({});
 
   const handleAnswer = (answer) => {
-    // Save the answer
-    setAnswers(prevAnswers => {
+    setAnswers((prevAnswers) => {
       const newAnswers = { ...prevAnswers, [currentQuestion]: answer };
-      // Move to the next question
       if (currentQuestion < questions.length - 1) {
-        setCurrentQuestion(prevIndex => prevIndex + 1);
+        setCurrentQuestion((prevIndex) => prevIndex + 1);
       } else {
-        setQuizStarted(false); // End quiz
+        setQuizStarted(false); // End the quiz
       }
       return newAnswers;
     });
   };
 
+  const handleBack = () => {
+    if (currentQuestion > 0) {
+      setCurrentQuestion((prevIndex) => prevIndex - 1);
+    }
+  };
+
   return (
-    <section className="bg-gray-200 min-h-screen pt-16 pb-8 text-center">
+    <section className="bg-gray-200 min-h-screen pt-16 pb-8 text-center relative">
       <div className="bg-gradient-to-r from-gray-100 to-lightblue h-screen flex flex-col justify-center items-center">
         {!quizStarted && (
           <>
-            <h2 className="text-center text-black text-4xl font-semibold">Quiz Page</h2>
-            <p className='mt-1 text-xs'>Answer the following questions to find personalized products!</p>
+            <h2 className="text-center text-black text-4xl font-semibold">
+              Quiz Page
+            </h2>
+            <p className="mt-1 text-xs">
+              Answer the following questions to find personalized products!
+            </p>
           </>
         )}
-        {quizStarted ? (
-          currentQuestion < questions.length ? (
-            <div>
-              <p className="text-2xl mb-4">{questions[currentQuestion].question}</p>
-              {questions[currentQuestion].options.map((option, index) => (
-                <button
-                  key={index}
-                  onClick={() => handleAnswer(option)}
-                  className="bg-blue text-white p-2 rounded m-2 hover:opacity-60"
-                >
-                  {option}
-                </button>
-              ))}
-            </div>
-          ) : (
-            <div>
-              <h3>Quiz Completed</h3>
-              {/* Display results or a message here */}
-            </div>
-          )
-        ) : (
-          <button onClick={() => setQuizStarted(true)} className="bg-blue text-white p-2 rounded mt-4 hover:opacity-70">
+        {quizStarted && (
+          <>
+            {currentQuestion > 0 && (
+              <button
+                onClick={handleBack}
+                className="absolute top-20 left-4 text-black p-2 rounded hover:bg-blue-700"
+              >
+                <MdOutlineKeyboardBackspace size={24} color="grey" />
+              </button>
+            )}
+            {currentQuestion < questions.length ? (
+              <div>
+                <p className="text-xl font-semibold">
+                  {questions[currentQuestion].question}
+                </p>
+                {questions[currentQuestion].options.map((option, index) => (
+                  <button
+                    key={index}
+                    onClick={() => handleAnswer(option)}
+                    className="bg-blue text-white p-2 rounded m-2 hover:opacity-60"
+                  >
+                    {option}
+                  </button>
+                ))}
+              </div>
+            ) : (
+              <div>
+                <h3>Quiz Completed</h3>
+                {/* Display results or a message here */}
+              </div>
+            )}
+          </>
+        )}
+        {!quizStarted && (
+          <button
+            onClick={() => setQuizStarted(true)}
+            className="bg-blue text-white p-2 rounded mt-4 hover:opacity-70"
+          >
             Start Quiz
           </button>
         )}
