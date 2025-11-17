4526f4e092fd6d9bd3677d5b406ed07f7a69906d
Edmer Valencia <edmerfranciz@gmail.com>
2025-11-16 23:58:15 -0600

    fix error bug

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,18 +1,20 @@
 import { FaYoutube, FaTiktok } from "react-icons/fa";
 import { Link } from "react-router-dom";
+
 const Footer = () => {
+  const scrollToTop = () => window.scrollTo(0,0);
   return (
     <div className="bg-gray-200 p-4 text-center">
       {/* Create a row for all footer items */}
       <div className="flex flex-wrap justify-around items-center text-xs sm:text-sm">
         <div className="p-2 sm:p-4 hover:text-blue hover:underline cursor-pointer">
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={scrollToTop}>Home</Link>
         </div>
         <div className="p-2 sm:p-4 hover:text-blue hover:underline cursor-pointer">
-          <Link to="/shop">Products</Link>
+          <Link to="/shop" onClick={scrollToTop}>Products</Link>
         </div>
         <div className="p-2 sm:p-4 hover:text-blue hover:underline cursor-pointer">
-          <Link to="/links">Personalized Gifts</Link>
+          <Link to="/links" onClick={scrollToTop}>Personalized Gifts</Link>
         </div>
         <div className="p-2 sm:p-4 hover:text-blue hover:underline cursor-pointer">
           <Link to="/contact">Contact Us</Link>
